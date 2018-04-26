d548f4e89b9143a87f0cc4e9107f5301483f252c
uni20162013 <bekjan298@mail.ru>
2018-04-26 13:51:07 +0900

    Centroid Decomposition

---
+++ centroidDecomposition.cpp
@@ -0,0 +1,47 @@
+/*
+Centroid Decomposition
+*/
+ll used[N+10],cnt[N+10];
+vector<ll>v[N+10],pr[N+10];
+void dfs(ll x, ll par)
+{
+	cnt[x]=1;
+	for(ll to:v[x])
+	{
+		if(to==par||used[to])
+			continue;
+		dfs(to,x);
+		cnt[x]+=cnt[to];
+	}
+}
+void dfs1(ll root, ll x, ll par)
+{
+	pr[x].pb(root);
+	for(ll to:v[x])
+	{
+		if(to==par||used[to])
+			continue;
+		dfs1(root,to,x);
+	}
+}
+void cen(ll x)
+{
+	dfs(x,0);
+	ll sz=cnt[x];
+	for(ll ch=1;ch;)
+	{
+		ch=0;
+		for(ll to:v[x])
+			if(cnt[to]<cnt[x]&&cnt[to]>sz/2)
+			{
+				ch=1;
+				x=to;
+				break;
+			}
+	}
+	used[x]=1;
+	dfs1(x,x,0);
+	for(ll to:v[x])
+		if(!used[to])
+			cen(to);
+}
