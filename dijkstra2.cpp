4ae27a3f18ab2d5f20a911316f75a019ecf5ff02
uni20162013 <bekjan298@mail.ru>
2018-04-28 00:26:31 +0900

    Дейкстра O(M+N^2)

--- dijkstra2.cpp
+++ dijkstra2.cpp
@@ -1,5 +1,5 @@
 /*
-Dijkstra's algorithm O(MlogN)
+Dijkstra's algorithm O(M+N^2)
 */
 ll dist[N+10],w[N+10];
 struct edge{
@@ -9,20 +9,20 @@ struct edge{
 vector<edge>v[N+10];
 void dijkstra()
 {
-	priority_queue<pair<ll,ll> >s;
 	for(ll i=1;i<=n;i++)
-		dist[i]=INF;
+		dist[i]=INF,w[i]=0;
 	dist[st]=0;
-	s.push({0,st});
-	for(;!s.empty();)
+	for(;;)
 	{
-		ll x=s.top().se;
-		s.pop();
-		if(w[x])
-			continue;
+		ll x=0;
+		for(ll i=1;i<=n;i++)
+			if(w[i]==0&&(x==0||dist[i]<dist[x]))
+				x=i;
+		if(x==0)
+			break;
 		w[x]=1;
 		for(edge e:v[x])
 			if(dist[e.x]+e.z<dist[e.y])
-				dist[e.y]=dist[e.x]+e.z,s.push({-dist[e.y],e.y});
+				dist[e.y]=dist[e.x]+e.z;
 	}
 }
