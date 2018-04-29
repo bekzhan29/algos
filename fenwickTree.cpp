df299a41458d26049adc47ff44ae24aa2183da68
uni20162013 <bekjan298@mail.ru>
2018-04-29 17:22:19 +0900

    Дерево Фенвика

---
+++ fenwickTree.cpp
@@ -0,0 +1,25 @@
+/*
+Дерево Фенвика O(NlogN)
+- Сумма на отрезке O(logN)
+- a[x] += y
+*/
+ll tree[N+10];
+void add(ll pos, ll x)
+{
+	for(;pos<=n;pos=(i|(i+1)))
+		tree[i]+=x;
+}
+ll sum(ll pos)
+{
+	ll ans=0;
+	for(;pos>=0;pos=(pos&(pos+1))-1)
+		ans+=tree[pos];
+	return ans;
+}
+void init()
+{
+	for(ll i=0;i<=n;i++)
+		tree[i]=0;
+	for(ll i=1;i<=n;i++)
+		add(i,a[i]);
+}
