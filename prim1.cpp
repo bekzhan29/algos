90edb5c0745c4bc966896ed485129aab1b320eea
uni20162013 <bekjan298@mail.ru>
2018-05-01 18:03:56 +0900

    Прима O(MlogN)

---
+++ prim1.cpp
@@ -0,0 +1,28 @@
+/*
+Prim's algorithm O(MlogN)
+*/
+ll dist[N+10],w[N+10],pr[N+10];
+struct edge{
+	ll x,y,z;
+	edge(ll x, ll y, ll z):x(x),y(y),z(z){}
+};
+vector<edge>v[N+10];
+void dijkstra()
+{
+	priority_queue<pair<ll,ll> >s;
+	for(ll i=1;i<=n;i++)
+		dist[i]=INF,w[i]=0;
+	dist[st]=0;
+	s.push({0,st});
+	for(;!s.empty();)
+	{
+		ll x=s.top().se;
+		s.pop();
+		if(w[x])
+			continue;
+		w[x]=1;
+		for(edge e:v[x])
+			if(e.z<dist[e.y])
+				dist[e.y]=e.z,pr[e.y]=e.x,s.push({-dist[e.y],e.y});
+	}
+}
