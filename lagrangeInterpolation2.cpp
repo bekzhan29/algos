5a7dcf673024f4decfcab21456ac83f945281e1a
uni20162013 <bekjan298@mail.ru>
2021-03-19 23:39:58 +0600

    Lagrange Interpolation O(NlogN)

--- lagrangeInterpolation2.cpp
+++ lagrangeInterpolation2.cpp
@@ -1,5 +1,5 @@
 /*
-Lagrange Interpolation O(KlogK)
+Lagrange Interpolation O(NlogN)
 1^k+2^k+...+x^k
 */
 #include <bits/stdc++.h>
@@ -56,11 +56,12 @@ int main()
 	cin.tie(0);
 
 	cin>>x>>k;
-	if(x<=k+2)
+	n=k+2;
+	if(x<=n)
 		return cout<<f(x), 0;
 	calc=1;
 	pr[0]=su[0]=1;
-	for(ll i=1;i<=k+2;i++)
+	for(ll i=1;i<=n;i++)
 	{
 		a[i]=a[i-1]+bin(i,k),a[i]%=mod;
 		pr[i]=pr[i-1]*i,pr[i]%=mod;
@@ -68,9 +69,9 @@ int main()
 		calc*=(x-i+mod)%mod;
 		calc%=mod;
 	}
-	for(ll i=1;i<=k+2;i++)
+	for(ll i=1;i<=n;i++)
 	{
-		cur=a[i]*calc%mod*bin((x-i+mod)%mod,mod-2)%mod*bin(pr[i-1],mod-2)%mod*bin(su[k+2-i],mod-2)%mod;
+		cur=a[i]*calc%mod*bin((x-i+mod)%mod,mod-2)%mod*bin(pr[i-1],mod-2)%mod*bin(su[n-i],mod-2)%mod;
 		ans+=cur;
 		ans%=mod;
 	}
