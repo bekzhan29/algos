fbe57511d136aed417cb34866452410f38f83b4c
bekzhan29 <bekjan298@mail.ru>
2023-02-01 15:24:47 +0000

    Количество разбиений числа N на слагаемые O(NsqrtN)

--- partitions_of_N.cpp
+++ partitions_of_N.cpp
@@ -17,30 +17,30 @@ using namespace std;
 #define INF 2e14
 #define mod 998244353
 #define eps 1e-9
-#define abs(x) ((x)>=0?(x):-(x))
+#define abs(x) ((x) >= 0 ? (x) : -(x))
 #define y1 solai
 #define fi first
 #define se second
 typedef long long ll;
 typedef long double ld;
-typedef pair<ll,ll> pll;
-const ll N=1000100;
-ll n,d[N];
+typedef pair<ll, ll> pll;
+const ll N = 1000100;
+ll n, d[N];
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin>>n;
-	d[0]=1;
-	for(ll i=1;i<=n;i++)
-		for(ll j=1;(3*j*j-j)/2<=i;j++)
+	cin >> n;
+	d[0] = 1;
+	for (ll i = 1; i <= n; i++)
+		for (ll j = 1; (3 * j * j - j) / 2 <= i; j++)
 		{
-			d[i]+=(j%2?1:-1)*d[i-(3*j*j-j)/2];
-			if((3*j*j+j)/2<=i)
-				d[i]+=(j%2?1:-1)*d[i-(3*j*j+j)/2];
-			d[i]%=mod;
+			d[i] += (j % 2 ? 1 : -1) * d[i - (3 * j * j - j) / 2];
+			if ((3 * j * j + j) / 2 <= i)
+				d[i] += (j % 2 ? 1 : -1) * d[i - (3 * j * j + j) / 2];
+			d[i] %= mod;
 		}
-	cout<<d[n];
+	cout << d[n];
 }
