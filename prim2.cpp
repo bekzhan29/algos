b53108fbaffa0a166e78b36c4c60abbcc09ae827
bekzhan29 <bekjan298@mail.ru>
2023-02-01 15:26:22 +0000

    Прима O(M+N^2)

--- prim2.cpp
+++ prim2.cpp
@@ -1,28 +1,29 @@
 /*
 Prim's algorithm O(M+N^2)
 */
-ll dist[N+10],w[N+10],pr[N+10];
-struct edge{
-	ll x,y,z;
-	edge(ll x, ll y, ll z):x(x),y(y),z(z){}
+ll dist[N + 10], w[N + 10], pr[N + 10];
+struct edge
+{
+	ll x, y, z;
+	edge(ll x, ll y, ll z) : x(x), y(y), z(z) {}
 };
-vector<edge>v[N+10];
+vector<edge> v[N + 10];
 void dijkstra()
 {
-	for(ll i=1;i<=n;i++)
-		dist[i]=INF,w[i]=0;
-	dist[1]=pr[1]=0;
-	for(;;)
+	for (ll i = 1; i <= n; i++)
+		dist[i] = INF, w[i] = 0;
+	dist[1] = pr[1] = 0;
+	for (;;)
 	{
-		ll x=0;
-		for(ll i=1;i<=n;i++)
-			if(w[i]==0&&(x==0||dist[i]<dist[x]))
-				x=i;
-		if(x==0)
+		ll x = 0;
+		for (ll i = 1; i <= n; i++)
+			if (w[i] == 0 && (x == 0 || dist[i] < dist[x]))
+				x = i;
+		if (x == 0)
 			break;
-		w[x]=1;
-		for(edge e:v[x])
-			if(e.z<dist[e.y])
-				dist[e.y]=e.z,pr[e.y]=e.x;
+		w[x] = 1;
+		for (edge e : v[x])
+			if (e.z < dist[e.y])
+				dist[e.y] = e.z, pr[e.y] = e.x;
 	}
 }
