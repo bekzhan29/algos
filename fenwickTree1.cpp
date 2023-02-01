c66e0da3c179919f8e192521082bb9d6eefb168d
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:32:45 +0000

    Дерево Фенвика O(NlogN)

--- fenwickTree1.cpp
+++ fenwickTree1.cpp
@@ -6,24 +6,24 @@
 ll tree[N];
 void add(ll pos, ll x)
 {
-	for(;pos<=n;pos=(pos|(pos+1)))
-		tree[pos]+=x;
+	for (; pos <= n; pos = (pos | (pos + 1)))
+		tree[pos] += x;
 }
 ll sum(ll pos)
 {
-	ll ans=0;
-	for(;pos>=0;pos=(pos&(pos+1))-1)
-		ans+=tree[pos];
+	ll ans = 0;
+	for (; pos >= 0; pos = (pos & (pos + 1)) - 1)
+		ans += tree[pos];
 	return ans;
 }
 ll sum(ll l, ll r)
 {
-	return sum(r)-sum(l-1);
+	return sum(r) - sum(l - 1);
 }
 void build()
 {
-	for(ll i=0;i<=n;i++)
-		tree[i]=0;
-	for(ll i=1;i<=n;i++)
-		add(i,a[i]);
+	for (ll i = 0; i <= n; i++)
+		tree[i] = 0;
+	for (ll i = 1; i <= n; i++)
+		add(i, a[i]);
 }
