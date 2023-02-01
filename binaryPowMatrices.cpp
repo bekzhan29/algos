f56569619129a3d4a13c287fe25fc1a867fa9d52
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:23:47 +0000

    Бинарное возведение матрицы в степень O(N^3*logM)

--- binaryPowMatrices.cpp
+++ binaryPowMatrices.cpp
@@ -3,34 +3,34 @@
 */
 void bpow(ll m)
 {
-    for (ll i = 1; i <= n; i++)
-        for (ll j = 1; j <= n; j++)
-            res[i][j] = (i == j);
-    while (m)
-    {
-        if (m & 1)
-        {
-            for (ll i = 1; i <= n; i++)
-                for (ll j = 1; j <= n; j++)
-                {
-                    c[i][j] = 0;
-                    for (ll k = 1; k <= n; k++)
-                        c[i][j] += res[i][k] * a[k][j], c[i][j] %= mod;
-                }
-            for (ll i = 1; i <= n; i++)
-                for (ll j = 1; j <= n; j++)
-                    res[i][j] = c[i][j];
-        }
-        for (ll i = 1; i <= n; i++)
-            for (ll j = 1; j <= n; j++)
-            {
-                c[i][j] = 0;
-                for (ll k = 1; k <= n; k++)
-                    c[i][j] += a[i][k] * a[k][j], c[i][j] %= mod;
-            }
-        for (ll i = 1; i <= n; i++)
-            for (ll j = 1; j <= n; j++)
-                a[i][j] = c[i][j];
-        m /= 2;
-    }
+	for (ll i = 1; i <= n; i++)
+		for (ll j = 1; j <= n; j++)
+			res[i][j] = (i == j);
+	while (m)
+	{
+		if (m & 1)
+		{
+			for (ll i = 1; i <= n; i++)
+				for (ll j = 1; j <= n; j++)
+				{
+					c[i][j] = 0;
+					for (ll k = 1; k <= n; k++)
+						c[i][j] += res[i][k] * a[k][j], c[i][j] %= mod;
+				}
+			for (ll i = 1; i <= n; i++)
+				for (ll j = 1; j <= n; j++)
+					res[i][j] = c[i][j];
+		}
+		for (ll i = 1; i <= n; i++)
+			for (ll j = 1; j <= n; j++)
+			{
+				c[i][j] = 0;
+				for (ll k = 1; k <= n; k++)
+					c[i][j] += a[i][k] * a[k][j], c[i][j] %= mod;
+			}
+		for (ll i = 1; i <= n; i++)
+			for (ll j = 1; j <= n; j++)
+				a[i][j] = c[i][j];
+		m /= 2;
+	}
 }
