c47e152620b9fe4ecb808a3343b27be24755e8cc
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:29:40 +0000

    Дейкстра O(M+N^2)

--- dijkstra2.cpp
+++ dijkstra2.cpp
@@ -1,28 +1,29 @@
 /*
 Dijkstra's algorithm O(M+N^2)
 */
-ll dist[N+10],w[N+10];
-struct edge{
-	ll x,y,z;
-	edge(ll x, ll y, ll z):x(x),y(y),z(z){}
+ll dist[N + 10], w[N + 10];
+struct edge
+{
+	ll x, y, z;
+	edge(ll x, ll y, ll z) : x(x), y(y), z(z) {}
 };
-vector<edge>v[N+10];
+vector<edge> v[N + 10];
 void dijkstra()
 {
-	for(ll i=1;i<=n;i++)
-		dist[i]=INF,w[i]=0;
-	dist[st]=0;
-	for(;;)
+	for (ll i = 1; i <= n; i++)
+		dist[i] = INF, w[i] = 0;
+	dist[st] = 0;
+	for (;;)
 	{
-		ll x=0;
-		for(ll i=1;i<=n;i++)
-			if(w[i]==0&&(x==0||dist[i]<dist[x]))
-				x=i;
-		if(x==0)
+		ll x = 0;
+		for (ll i = 1; i <= n; i++)
+			if (w[i] == 0 && (x == 0 || dist[i] < dist[x]))
+				x = i;
+		if (x == 0)
 			break;
-		w[x]=1;
-		for(edge e:v[x])
-			if(dist[e.x]+e.z<dist[e.y])
-				dist[e.y]=dist[e.x]+e.z;
+		w[x] = 1;
+		for (edge e : v[x])
+			if (dist[e.x] + e.z < dist[e.y])
+				dist[e.y] = dist[e.x] + e.z;
 	}
 }
