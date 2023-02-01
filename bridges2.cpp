3febe6eff3c08d19b3d60fed440279e5c6f81fe2
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:24:49 +0000

    Поиск мостов с кратными рёбрами O(N+M)

--- bridges2.cpp
+++ bridges2.cpp
@@ -2,29 +2,30 @@
 Поиск мостов в связном графе O(N+M)
 Граф может содержать кратные рёбра
 */
-struct edge{
-	ll x,y,ind;
-	edge(ll x=0, ll y=0, ll ind=0):x(x),y(y),ind(ind){}
-}br[N+10];
-ll cnt,w[N+10],dep[N+10],up[N+10];
-vector<edge>v[N+10];
+struct edge
+{
+	ll x, y, ind;
+	edge(ll x = 0, ll y = 0, ll ind = 0) : x(x), y(y), ind(ind) {}
+} br[N + 10];
+ll cnt, w[N + 10], dep[N + 10], up[N + 10];
+vector<edge> v[N + 10];
 void dfs(ll x, ll last, ll depth)
 {
-	dep[x]=up[x]=depth;
-	w[x]=1;
-	for(edge e:v[x])
+	dep[x] = up[x] = depth;
+	w[x] = 1;
+	for (edge e : v[x])
 	{
-		if(e.ind==last)
+		if (e.ind == last)
 			continue;
-		ll to=e.y;
-		if(!w[to])
+		ll to = e.y;
+		if (!w[to])
 		{
-			dfs(to,e.ind,depth+1);
-			up[x]=min(up[x],up[to]);
-			if(up[to]>dep[x])
-				br[++cnt]=e;
+			dfs(to, e.ind, depth + 1);
+			up[x] = min(up[x], up[to]);
+			if (up[to] > dep[x])
+				br[++cnt] = e;
 		}
 		else
-			up[x]=min(up[x],dep[to]);
+			up[x] = min(up[x], dep[to]);
 	}
 }
