4d3c7b9a44a422fea1e53b82afb5876bb0fb5443
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:28:38 +0000

    Поиск точек сочленения O(N+M)

--- cutVertex.cpp
+++ cutVertex.cpp
@@ -2,30 +2,30 @@
 Поиск точек сочленения в связном графе O(N+M)
 Граф может содержать кратные рёбра
 */
-ll cnt,w[N+10],dep[N+10],up[N+10],cut[N+10];
-vector<ll>v[N+10];
+ll cnt, w[N + 10], dep[N + 10], up[N + 10], cut[N + 10];
+vector<ll> v[N + 10];
 void dfs(ll x, ll pr, ll depth)
 {
-	dep[x]=up[x]=depth;
-	w[x]=1;
-	ll ch=0,go=0;
-	for(ll to:v[x])
+	dep[x] = up[x] = depth;
+	w[x] = 1;
+	ll ch = 0, go = 0;
+	for (ll to : v[x])
 	{
-		if(to==pr)
+		if (to == pr)
 			continue;
-		if(!w[to])
+		if (!w[to])
 		{
 			go++;
-			dfs(to,x,depth+1);
-			up[x]=min(up[x],up[to]);
-			if(up[to]>=dep[x])
-				ch=1;
+			dfs(to, x, depth + 1);
+			up[x] = min(up[x], up[to]);
+			if (up[to] >= dep[x])
+				ch = 1;
 		}
 		else
-			up[x]=min(up[x],dep[to]);
+			up[x] = min(up[x], dep[to]);
 	}
-	if(pr==0)
-		ch=go>1;
-	if(ch)
-		cut[++cnt]=x;
+	if (pr == 0)
+		ch = go > 1;
+	if (ch)
+		cut[++cnt] = x;
 }
