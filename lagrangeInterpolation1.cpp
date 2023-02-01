9b4ef144063ec0d4ee73aa724a0b908760635b3a
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:39:42 +0000

    Lagrange Interpolation O(N^2logN)

--- lagrangeInterpolation1.cpp
+++ lagrangeInterpolation1.cpp
@@ -21,33 +21,33 @@ using namespace std;
 #define INF ll(2e15)
 #define mod 998244353
 #define eps 1e-9
-#define abs(x) ((x)>=0?(x):-(x))
+#define abs(x) ((x) >= 0 ? (x) : -(x))
 #define y1 solai
 #define fi first
 #define se second
 typedef long long ll;
 typedef long double ld;
-typedef pair<ll,ll> pll;
-typedef pair<double,double> pdd;
-const ll N=5100;
-ll n,k,a[N],x,cur,ans;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+const ll N = 5100;
+ll n, k, a[N], x, cur, ans;
 ll bin(ll a, ll n)
 {
-	ll ans=1;
-	while(n)
+	ll ans = 1;
+	while (n)
 	{
-		if(n&1)
-			ans*=a,ans%=mod;
-		a*=a,a%=mod;
-		n/=2;
+		if (n & 1)
+			ans *= a, ans %= mod;
+		a *= a, a %= mod;
+		n /= 2;
 	}
 	return ans;
 }
 ll f(ll x)
 {
-	ll ans=0;
-	for(ll i=1;i<=x;i++)
-		ans+=bin(i,k),ans%=mod;
+	ll ans = 0;
+	for (ll i = 1; i <= x; i++)
+		ans += bin(i, k), ans %= mod;
 	return ans;
 }
 int main()
@@ -55,18 +55,18 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin>>x>>k;
-	n=k+2;
-	for(ll i=1;i<=n;i++)
-		a[i]=a[i-1]+bin(i,k),a[i]%=mod;
-	for(ll i=1;i<=n;i++)
+	cin >> x >> k;
+	n = k + 2;
+	for (ll i = 1; i <= n; i++)
+		a[i] = a[i - 1] + bin(i, k), a[i] %= mod;
+	for (ll i = 1; i <= n; i++)
 	{
-		cur=a[i];
-		for(ll j=1;j<=n;j++)
-			if(i!=j)
-				cur=cur*(x-j+mod)%mod*bin((i-j+mod)%mod,mod-2)%mod;
-		ans+=cur;
-		ans%=mod;
+		cur = a[i];
+		for (ll j = 1; j <= n; j++)
+			if (i != j)
+				cur = cur * (x - j + mod) % mod * bin((i - j + mod) % mod, mod - 2) % mod;
+		ans += cur;
+		ans %= mod;
 	}
-	cout<<ans;
+	cout << ans;
 }
