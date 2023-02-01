3423bc9200ce560b8a3a59c1684115af200ee170
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:35:53 +0000

    Алгоритм поиска выпуклой оболочки O(NlogN)

--- grahamConvexHull.cpp
+++ grahamConvexHull.cpp
@@ -1,38 +1,38 @@
 struct point
 {
-	ll x,y;
-	bool operator==(const point& a)
+	ll x, y;
+	bool operator==(const point &a)
 	{
-		return x==a.x&&y==a.y;
+		return x == a.x && y == a.y;
 	}
-	bool operator<(const point& a)
+	bool operator<(const point &a)
 	{
-		if(x!=a.x)
-			return x<a.x;
-		return y<a.y;
+		if (x != a.x)
+			return x < a.x;
+		return y < a.y;
 	}
-}a[N];
+} a[N];
 ll cross(point o, point a, point b)
 {
-	return (a.x-o.x)*(b.y-o.y)-(a.y-o.y)*(b.x-o.x);
+	return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
 }
 vector<point> convexHull(ll n, point a[])
 {
-	vector<point>ans;
-	sort(a+1,a+n+1);
-	ll sz=0;
-	for(ll i=1;i<=n;i++)
+	vector<point> ans;
+	sort(a + 1, a + n + 1);
+	ll sz = 0;
+	for (ll i = 1; i <= n; i++)
 	{
-		while(sz>=2&&cross(ans[sz-1],ans[sz-2],a[i])>=0)
-			ans.pop_back(),sz--;
+		while (sz >= 2 && cross(ans[sz - 1], ans[sz - 2], a[i]) >= 0)
+			ans.pop_back(), sz--;
 		ans.pb(a[i]);
 		sz++;
 	}
-	ll t=sz;
-	for(ll i=n-1;i>=1;i--)
+	ll t = sz;
+	for (ll i = n - 1; i >= 1; i--)
 	{
-		while(sz>t&&cross(ans[sz-1],ans[sz-2],a[i])>=0)
-			ans.pop_back(),sz--;
+		while (sz > t && cross(ans[sz - 1], ans[sz - 2], a[i]) >= 0)
+			ans.pop_back(), sz--;
 		ans.pb(a[i]);
 		sz++;
 	}
