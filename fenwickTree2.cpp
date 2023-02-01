1e23abaf4d5cacf271756761195e88caacfcd5c1
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:33:15 +0000

    upd(l,r)+=k, sum(l,r)

--- fenwickTree2.cpp
+++ fenwickTree2.cpp
@@ -6,31 +6,31 @@
 ll tree[2][N];
 void add2(ll ty, ll pos, ll x)
 {
-	for(;pos<=n;pos=(pos|(pos+1)))
-		tree[ty][pos]+=x;
+	for (; pos <= n; pos = (pos | (pos + 1)))
+		tree[ty][pos] += x;
 }
 void add(ll l, ll r, ll x)
 {
-	add2(0,l,x);
-	add2(0,r+1,-x);
-	add2(1,l,(1-l)*x);
-	add2(1,r+1,r*x);
+	add2(0, l, x);
+	add2(0, r + 1, -x);
+	add2(1, l, (1 - l) * x);
+	add2(1, r + 1, r * x);
 }
 ll sum2(ll ty, ll pos)
 {
-	ll ans=0;
-	for(;pos>=0;pos=(pos&(pos+1))-1)
-		ans+=tree[ty][pos];
+	ll ans = 0;
+	for (; pos >= 0; pos = (pos & (pos + 1)) - 1)
+		ans += tree[ty][pos];
 	return ans;
 }
 ll sum(ll l, ll r)
 {
-	return sum2(0,r)*r-sum2(0,l-1)*(l-1)+sum2(1,r)-sum2(1,l-1);
+	return sum2(0, r) * r - sum2(0, l - 1) * (l - 1) + sum2(1, r) - sum2(1, l - 1);
 }
 void build()
 {
-	for(ll i=0;i<=n;i++)
-		tree[0][i]=tree[1][i]=0;
-	for(ll i=1;i<=n;i++)
-		add2(1,i,a[i]);
+	for (ll i = 0; i <= n; i++)
+		tree[0][i] = tree[1][i] = 0;
+	for (ll i = 1; i <= n; i++)
+		add2(1, i, a[i]);
 }
