cecb9a88ee7774d4758cfea4e511b43a6356498c
bekzhan29 <bekjan298@mail.ru>
2023-02-01 15:22:41 +0000

    Метод двоичного подъема O(logN)

--- lca1.cpp
+++ lca1.cpp
@@ -2,33 +2,33 @@
 Наименьший общий предок
 Поиск двоичным подьемом O(logN)
 */
-const ll lg=17;
-ll pr[lg][N],tin[N],tout[N],tim;
-vector<ll>v[N];
+const ll lg = 17;
+ll pr[lg][N], tin[N], tout[N], tim;
+vector<ll> v[N];
 void dfs(ll x, ll par)
 {
-	tin[x]=++tim;
-	pr[0][x]=par;
-	if(par==0)
-		pr[0][x]=x;
-	for(ll i=1;i<lg;i++)
-		pr[i][x]=pr[i-1][pr[i-1][x]];
-	for(ll to:v[x])
-		if(to!=par)
-			dfs(to,x);
-	tout[x]=tim;
+	tin[x] = ++tim;
+	pr[0][x] = par;
+	if (par == 0)
+		pr[0][x] = x;
+	for (ll i = 1; i < lg; i++)
+		pr[i][x] = pr[i - 1][pr[i - 1][x]];
+	for (ll to : v[x])
+		if (to != par)
+			dfs(to, x);
+	tout[x] = tim;
 }
 ll lca(ll x, ll y)
 {
-	ll ans=x;
-	for(ll i=lg-1;i>=0;i--)
-		if(tin[pr[i][ans]]>tin[y]||tout[y]>tout[pr[i][ans]])
-			ans=pr[i][lca];
-	if(tin[ans]>tin[y]||tout[y]>tout[ans])
-		ans=pr[0][ans];
+	ll ans = x;
+	for (ll i = lg - 1; i >= 0; i--)
+		if (tin[pr[i][ans]] > tin[y] || tout[y] > tout[pr[i][ans]])
+			ans = pr[i][lca];
+	if (tin[ans] > tin[y] || tout[y] > tout[ans])
+		ans = pr[0][ans];
 	return ans;
 }
 void init(ll root)
 {
-	dfs(root,0);
+	dfs(root, 0);
 }
