5825d5da7412c099e42d4284414a663bee9cb06c
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:33:45 +0000

    Алгоритм Флойда-Уоршелла O(N^3)

--- floydWarshall.cpp
+++ floydWarshall.cpp
@@ -1,19 +1,19 @@
 /*
 Floyd-Warshall O(N^3)
 */
-ll x[N*N],y[N*N],z[N*N],dist[N][N];
+ll x[N * N], y[N * N], z[N * N], dist[N][N];
 void floydWarshall()
 {
-	for(ll i=1;i<=n;i++)
-		for(ll j=1;j<=n;j++)
-			dist[i][j]=INF;
-	for(ll i=1;i<=n;i++)
-		dist[i][i]=0;
-	for(ll i=1;i<=m;i++)
-		dist[x[i]][y[i]]=z[i],
-		dist[y[i]][x[i]]=z[i];
-	for(ll k=1;k<=n;k++)
-		for(ll i=1;i<=n;i++)
-			for(ll j=1;j<=n;j++)
-				dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+	for (ll i = 1; i <= n; i++)
+		for (ll j = 1; j <= n; j++)
+			dist[i][j] = INF;
+	for (ll i = 1; i <= n; i++)
+		dist[i][i] = 0;
+	for (ll i = 1; i <= m; i++)
+		dist[x[i]][y[i]] = z[i],
+		dist[y[i]][x[i]] = z[i];
+	for (ll k = 1; k <= n; k++)
+		for (ll i = 1; i <= n; i++)
+			for (ll j = 1; j <= n; j++)
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 }
