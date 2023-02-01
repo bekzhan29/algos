6edb86570c01635aa17930815d7bc197c5f43a4f
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:27:13 +0000

    Китайская теорема об остатках O(N)

--- chineseRemainderTheorem.cpp
+++ chineseRemainderTheorem.cpp
@@ -5,43 +5,44 @@
 m=k[1]*k[2]*...*k[sz]
 0<=x<m
 */
-ll sz,k[N],mi[N],rmi[N],re[N];
+ll sz, k[N], mi[N], rmi[N], re[N];
 ll bin(ll a, ll n, ll m)
 {
-	ll ans=1;
-	while(n)
+	ll ans = 1;
+	while (n)
 	{
-		if(n&1)
-			ans*=a,ans%=m;
-		a*=a,a%=m;
-		n/=2;
+		if (n & 1)
+			ans *= a, ans %= m;
+		a *= a, a %= m;
+		n /= 2;
 	}
 	return ans;
 }
-ll phi(ll n) {
-	ll ans=n;
-	for(ll i=2;i*i<=n;i++)
-		if(n%i==0)
+ll phi(ll n)
+{
+	ll ans = n;
+	for (ll i = 2; i * i <= n; i++)
+		if (n % i == 0)
 		{
-			while(n%i==0)
-				n/=i;
-			ans-=ans/i;
+			while (n % i == 0)
+				n /= i;
+			ans -= ans / i;
 		}
-	if(n>1)
-		ans-=ans/n;
+	if (n > 1)
+		ans -= ans / n;
 	return ans;
 }
 ll CRM()
 {
-	ll m=1,ans=0;
-	for(ll i=1;i<=sz;i++)
-		m*=k[i];
-	for(ll i=1;i<=sz;i++)
+	ll m = 1, ans = 0;
+	for (ll i = 1; i <= sz; i++)
+		m *= k[i];
+	for (ll i = 1; i <= sz; i++)
 	{
-		mi[i]=m/k[i];
-		rmi[i]=bin(mi[i],phi(k[i])-1,k[i]);
-		ans+=re[i]*rmi[i]*mi[i];
-		ans%=m;
+		mi[i] = m / k[i];
+		rmi[i] = bin(mi[i], phi(k[i]) - 1, k[i]);
+		ans += re[i] * rmi[i] * mi[i];
+		ans %= m;
 	}
 	return ans;
 }
