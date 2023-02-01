270cc17b39f8183e9ab2b40451320e26c777f124
bekzhan29 <bekjan298@mail.ru>
2023-02-01 15:28:45 +0000

    Slope Trick O(NlogN)

--- slopeTrick.cpp
+++ slopeTrick.cpp
@@ -22,30 +22,30 @@ using namespace std;
 #define INF ll(2e9)
 #define mod 998244353
 #define eps 1e-9
-#define abs(x) ((x)>=0?(x):-(x))
+#define abs(x) ((x) >= 0 ? (x) : -(x))
 #define y1 solai
 #define fi first
 #define se second
 typedef long long ll;
 typedef long double ld;
-typedef pair<ll,ll> pll;
-typedef pair<double,double> pdd;
-const ll N=100100;
-ll n,x,ans;
-priority_queue<ll>q;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+const ll N = 100100;
+ll n, x, ans;
+priority_queue<ll> q;
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin>>n;
-	for(ll i=1;i<=n;i++)
+	cin >> n;
+	for (ll i = 1; i <= n; i++)
 	{
-		cin>>x,x-=i;
+		cin >> x, x -= i;
 		q.push(x);
 		q.push(x);
-		ans+=q.top()-x;
+		ans += q.top() - x;
 		q.pop();
 	}
-	cout<<ans;
+	cout << ans;
 }
