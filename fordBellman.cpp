f05fb09c07be6262fc7e82d3e9557fa162ab62ce
bekzhan29 <bekjan298@mail.ru>
2023-02-01 12:34:18 +0000

    Форд-Беллман O(NM)

--- fordBellman.cpp
+++ fordBellman.cpp
@@ -1,22 +1,23 @@
 /*
 Bellman-Ford's algorithm O(NM)
 */
-ll dist[N+10];
-struct edge{
-	ll x,y,z;
-	edge(ll x, ll y, ll z):x(x),y(y),z(z){}
-}e[N+10];
+ll dist[N + 10];
+struct edge
+{
+	ll x, y, z;
+	edge(ll x, ll y, ll z) : x(x), y(y), z(z) {}
+} e[N + 10];
 void fordBellman()
 {
-	for(ll i=1;i<=n;i++)
-		dist[i]=INF;
-	dist[st]=0;
-	for(ll i=1;i<=n;i++)
-		for(ll j=1;j<=m;j++)
-			if(dist[e.y]>dist[e.x]+e.z)
+	for (ll i = 1; i <= n; i++)
+		dist[i] = INF;
+	dist[st] = 0;
+	for (ll i = 1; i <= n; i++)
+		for (ll j = 1; j <= m; j++)
+			if (dist[e.y] > dist[e.x] + e.z)
 			{
-				dist[e.y]=dist[e.x]+e.z;
-				if(i==n)
-					cout<<"Negative weight cycle"
+				dist[e.y] = dist[e.x] + e.z;
+				if (i == n)
+					cout << "Negative weight cycle"
 			}
 }
