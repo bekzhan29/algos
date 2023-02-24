e2e9be6f5e65ce6ef7c4fcdee7b1ac55f2793b23
bekzhan29 <bekjan298@mail.ru>
2023-02-24 14:39:48 +0000

    Прима O(MlogN)

--- Prim/prim1.cpp
+++ Prim/prim1.cpp
@@ -8,13 +8,13 @@ struct edge
 	edge(ll x = 0, ll y = 0, ll w = 0) : x(x), y(y), w(w) {}
 };
 vector<edge> v[N];
-void prim(ll st, ll n)
+void prim(ll n)
 {
 	priority_queue<pair<ll, ll>> s;
 	for (ll i = 1; i <= n; i++)
 		dist[i] = INF, w[i] = 0;
-	dist[st] = 0;
-	s.push({0, st});
+	dist[1] = 0;
+	s.push({0, 1});
 	for (; !s.empty();)
 	{
 		ll x = s.top().se;
@@ -23,7 +23,7 @@ void prim(ll st, ll n)
 			continue;
 		w[x] = 1;
 		for (edge e : v[x])
-			if (e.w < dist[e.y])
+			if (!w[e.y] && e.w < dist[e.y])
 				dist[e.y] = e.w, pr[e.y] = e.x, s.push({-dist[e.y], e.y});
 	}
 }
