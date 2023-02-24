0fe4c45c1cfd2124804b4ae2cc3d048c6933fd71
bekzhan29 <bekjan298@mail.ru>
2023-02-24 23:04:52 +0000

    Disjoint Set Union O(1)

--- dsu.cpp
+++ dsu.cpp
@@ -1,24 +1,28 @@
 /*
 Disjoint Set Union O(1)
+- init_dsu() O(N)
+- fin() O(1)
+- uni() O(1)
 */
-ll p[N + 10], r[N + 10];
+ll p[N], r[N];
 ll fin(ll a)
 {
 	if (a == p[a])
 		return a;
 	return p[a] = fin(p[a]);
 }
-void uni(ll a, ll b)
+bool uni(ll a, ll b)
 {
 	a = fin(a), b = fin(b);
 	if (a == b)
-		return;
+		return 0;
 	if (r[a] > r[b])
 		r[a] += r[b], p[b] = a;
 	else
 		r[b] += r[a], p[a] = b;
+	return 1;
 }
-void dsu()
+void init_dsu(ll n)
 {
 	for (ll i = 1; i <= n; i++)
 		p[i] = i, r[i] = 1;
