42a296380849cb38cd7877e32e55e52a25657769
bekzhan29 <bekjan298@mail.ru>
2023-03-06 18:17:08 +0000

    a[pos]+=k, sum(l,r)

--- Sqrt Decomposition/sqrtDecomposition1.cpp
+++ Sqrt Decomposition/sqrtDecomposition1.cpp
@@ -3,31 +3,41 @@ Sqrt Decomposition
 - a[x] += y O(1)
 - sum(l,r) O(sqrt(N))
 */
-ll bl[N], len = 320;
-void build()
+template <typename Type>
+struct sqrt_dec
 {
-	for (ll i = 1; i <= n; i++)
-		bl[i / len] += a[i];
-}
-void upd(ll pos, ll k)
-{
-	a[pos] += k;
-	bl[pos / len] += k;
-}
-ll sum(ll l, ll r)
-{
-	ll cl = l / len, cr = r / len, ans = 0;
-	if (cl == cr)
+	ll len = 320;
+	Type a[N], bl[N];
+	void build(ll n, Type *a)
 	{
-		for (ll i = l; i <= r; i++)
+		len = sqrt(n);
+		for (ll i = 1; i <= n; i++)
+		{
+			this->a[i] = a[i];
+			bl[i / len] += a[i];
+		}
+	}
+	void upd(ll pos, Type k)
+	{
+		a[pos] += k;
+		bl[pos / len] += k;
+	}
+	Type sum(ll l, ll r)
+	{
+		Type ans = 0;
+		ll cl = l / len, cr = r / len;
+		if (cl == cr)
+		{
+			for (ll i = l; i <= r; i++)
+				ans += a[i];
+			return ans;
+		}
+		for (ll i = l; i < (cl + 1) * len; i++)
+			ans += a[i];
+		for (ll i = cl + 1; i < cr; i++)
+			ans += bl[i];
+		for (ll i = cr * len; i <= r; i++)
 			ans += a[i];
 		return ans;
 	}
-	for (ll i = l; i < (cl + 1) * len; i++)
-		ans += a[i];
-	for (ll i = cl + 1; i < cr; i++)
-		ans += bl[i];
-	for (ll i = cr * len; i <= r; i++)
-		ans += a[i];
-	return ans;
-}
+};
