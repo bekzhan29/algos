30a4e394d00327c088763f016fbe627c628b0fa2
bekzhan29 <bekjan298@mail.ru>
2023-03-07 22:46:18 +0000

    Сверху вниз upd(l,r)+=k, sum(l,r)

--- Segment Tree/segmentTree2.cpp
+++ Segment Tree/segmentTree2.cpp
@@ -6,9 +6,10 @@
 template <typename Type>
 struct segment_tree
 {
+private:
 	ll n;
 	vector<Type> tree, z;
-	void build(ll v, ll l, ll r, Type *a)
+	void build_tree(ll v, ll l, ll r, Type *a)
 	{
 		if (l == r)
 		{
@@ -17,8 +18,8 @@ struct segment_tree
 			return;
 		}
 		ll mid = (l + r) / 2;
-		build(v * 2, l, mid, a);
-		build(v * 2 + 1, mid + 1, r, a);
+		build_tree(v * 2, l, mid, a);
+		build_tree(v * 2 + 1, mid + 1, r, a);
 		tree[v] = tree[v * 2] + tree[v * 2 + 1];
 		z[v] = 0;
 	}
@@ -32,7 +33,7 @@ struct segment_tree
 		tree[v * 2 + 1] += z[v] * (r - mid);
 		z[v] = 0;
 	}
-	void upd(ll v, ll l, ll r, ll x, ll y, Type k)
+	void upd_tree(ll v, ll l, ll r, ll x, ll y, Type k)
 	{
 		if (x > y || x > r || y < l)
 			return;
@@ -44,11 +45,11 @@ struct segment_tree
 		}
 		ll mid = (l + r) / 2;
 		push(v, l, r, mid);
-		upd(v * 2, l, mid, x, y, k);
-		upd(v * 2 + 1, mid + 1, r, x, y, k);
+		upd_tree(v * 2, l, mid, x, y, k);
+		upd_tree(v * 2 + 1, mid + 1, r, x, y, k);
 		tree[v] = tree[v * 2] + tree[v * 2 + 1];
 	}
-	Type sum(ll v, ll l, ll r, ll x, ll y)
+	Type get_sum(ll v, ll l, ll r, ll x, ll y)
 	{
 		if (x > y || x > r || y < l)
 			return 0;
@@ -56,21 +57,23 @@ struct segment_tree
 			return tree[v];
 		ll mid = (l + r) / 2;
 		push(v, l, r, mid);
-		return sum(v * 2, l, mid, x, y) + sum(v * 2 + 1, mid + 1, r, x, y);
+		return get_sum(v * 2, l, mid, x, y) + get_sum(v * 2 + 1, mid + 1, r, x, y);
 	}
+
+public:
 	void build(ll n, Type *a)
 	{
 		this->n = n;
 		tree.resize(4 * n);
 		z.resize(4 * n);
-		build(1, 1, n, a);
+		build_tree(1, 1, n, a);
 	}
 	void upd(ll l, ll r, Type k)
 	{
-		upd(1, 1, n, l, r, k);
+		upd_tree(1, 1, n, l, r, k);
 	}
 	Type sum(ll l, ll r)
 	{
-		return sum(1, 1, n, l, r);
+		return get_sum(1, 1, n, l, r);
 	}
 };
