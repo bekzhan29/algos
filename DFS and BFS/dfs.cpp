06f23483bdf860885045274b37443b7e3b1b93e3
bekzhan29 <bekjan298@mail.ru>
2023-03-11 19:38:04 +0000

    Добавлен DFS

---
+++ DFS and BFS/dfs.cpp
@@ -0,0 +1,33 @@
+/*
+Depth First Search
+0 indexed graph
+*/
+struct graph
+{
+	int n;
+	vector<vector<int>> g;
+	vector<bool> used;
+	vector<int> d;
+	graph(int n)
+	{
+		this->n = n;
+		g = vector<vector<int>>(n, vector<int>(0));
+		used = vector<bool>(n, false);
+		d = vector<int>(n, -1);
+	}
+	void add_edge(int v, int u, bool directional)
+	{
+		g[v].push_back(u);
+		if (!directional)
+			g[u].push_back(v);
+	}
+	void dfs(int v, int p = -1)
+	{
+		used[v] = true;
+		for (int to : g[v])
+		{
+			if (!used[to])
+				dfs(to, v);
+		}
+	}
+};
