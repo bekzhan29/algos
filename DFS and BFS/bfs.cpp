9044664b509b9c980e7af768306af006323af6a9
bekzhan29 <bekjan298@mail.ru>
2023-03-11 19:37:57 +0000

    Добавлен BFS

---
+++ DFS and BFS/bfs.cpp
@@ -0,0 +1,43 @@
+/*
+Breadth First Search
+0 based graph
+*/
+struct graph
+{
+	int n;
+	vector<vector<int>> g;
+	vector<bool> used;
+	vector<int> d;
+	graph(int n)
+	{
+		this->n = n;
+		g = vector<vector<int>>(n, vector<int>(0));
+		used = vector<bool>(n, false);
+		d = vector<int>(n, -1);
+	}
+	void add_edge(int v, int u, bool directional)
+	{
+		g[v].push_back(u);
+		if (!directional)
+			g[u].push_back(v);
+	}
+	void bfs(int start)
+	{
+		queue<int> q;
+		q.push(start);
+		d[start] = 0;
+		while (!q.empty())
+		{
+			int v = q.front();
+			q.pop();
+			for (int to : g[v])
+			{
+				if (d[to] == -1)
+				{
+					d[to] = d[v] + 1;
+					q.push(to);
+				}
+			}
+		}
+	}
+};
