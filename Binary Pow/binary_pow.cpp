812adba349440b519412896a1579cf0a7868bb1d
bekzhan29 <bekjan298@mail.ru>
2023-03-12 22:00:19 +0000

    Change ll to int

--- Binary Pow/binary_pow.cpp
+++ Binary Pow/binary_pow.cpp
@@ -1,12 +1,12 @@
 int bin_pow(int a, int n, int m)
 {
-    ll ans = 1;
-    while (n)
-    {
-        if (n & 1)
-            ans *= a, ans %= m;
-        a = 1LL * a * a % m;
-        n /= 2;
-    }
-    return ans;
+	int ans = 1;
+	while (n)
+	{
+		if (n & 1)
+			ans = 1LL * ans * a % m;
+		a = 1LL * a * a % m;
+		n /= 2;
+	}
+	return ans;
 }
