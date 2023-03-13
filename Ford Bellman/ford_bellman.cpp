bfc938aa59005af3458d0dc5bee45d4e40c7adea
bekzhan29 <bekjan298@mail.ru>
2023-03-13 22:56:32 +0000

    Добавлена оптимизация

--- Ford Bellman/ford_bellman.cpp
+++ Ford Bellman/ford_bellman.cpp
@@ -9,17 +9,23 @@ struct edge
 	ll x, y, w;
 	edge(ll x = 0, ll y = 0, ll w = 0) : x(x), y(y), w(w) {}
 } e[N];
-void fordBellman(ll st, ll n, ll m)
+void ford_bellman(int st, int n, int m)
 {
-	for (ll i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 		dist[i] = INF;
 	dist[st] = 0;
-	for (ll i = 1; i <= n; i++)
-		for (ll j = 1; j <= m; j++)
+	for (int i = 1; i <= n; i++)
+	{
+		bool optimized = false;
+		for (int j = 1; j <= m; j++)
 			if (dist[e.y] > dist[e.x] + e.w)
 			{
+				optimized = true;
 				dist[e.y] = dist[e.x] + e.w;
 				if (i == n)
 					cout << "Negative weight cycle"
 			}
+		if (!optimized)
+			break;
+	}
 }
