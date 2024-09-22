8b9bae057f3234f2cea7e3ed1475e55be28b999d
bekzhan29 <bekjan298@mail.ru>
2024-09-22 20:52:04 +0100

    Улучшен код СНМ

--- Graph Theory/Disjoint Set Union/dsu.cpp
+++ Graph Theory/Disjoint Set Union/dsu.cpp
@@ -1,24 +1,29 @@
-ll p[N], r[N];
-ll fin(ll a)
+struct dsu
 {
-	// https://github.com/bekzhan29/algos
-	if (a == p[a])
-		return a;
-	return p[a] = fin(p[a]);
-}
-bool uni(ll a, ll b)
-{
-	a = fin(a), b = fin(b);
-	if (a == b)
-		return 0;
-	if (r[a] > r[b])
-		r[a] += r[b], p[b] = a;
-	else
-		r[b] += r[a], p[a] = b;
-	return 1;
-}
-void init_dsu(ll n)
-{
-	for (ll i = 1; i <= n; i++)
-		p[i] = i, r[i] = 1;
-}
+	vector<int> p, r;
+	int fin(int a)
+	{
+		// https://github.com/bekzhan29/algos
+		if (a == p[a])
+			return a;
+		return p[a] = fin(p[a]);
+	}
+	bool union_sets(int a, int b)
+	{
+		a = fin(a), b = fin(b);
+		if (a == b)
+			return 0;
+		if (r[a] > r[b])
+			r[a] += r[b], p[b] = a;
+		else
+			r[b] += r[a], p[a] = b;
+		return 1;
+	}
+	void init_dsu(int n)
+	{
+		p.resize(n + 1);
+		r.resize(n + 1);
+		for (int i = 1; i <= n; i++)
+			p[i] = i, r[i] = 1;
+	}
+};
