a250f2e3827bd98f6c378b05a6e075d07e12fcbe
bekzhan29 <bekjan298@mail.ru>
2024-09-29 16:15:43 +0100

    Упрощена реализация HLD

--- Graph Theory/Heavy Light Decomposition/node_hld.cpp
+++ Graph Theory/Heavy Light Decomposition/node_hld.cpp
@@ -3,12 +3,12 @@ struct hld
 {
 	// https://github.com/bekzhan29/algos
 private:
-	int n, cnt, sz;
-	vector<int> pr, pos, head, ind, l, r, c;
+	int n, sz;
+	vector<int> pr, pos, h, c;
 	// https://github.com/bekzhan29/algos/blob/master/Data%20Structures/Segment%20Tree/segment_tree1.cpp
 	segment_tree<Type> tree;
 	vector<vector<int>> v;
-	void dfs(int x, int par)
+	void dfs(int x, int par=0)
 	{
 		pr[x] = par;
 		c[x] = 1;
@@ -18,13 +18,12 @@ private:
 			if (to != par)
 				dfs(to, x), c[x] += c[to];
 	}
-	void hld_build(int x, int par)
+	void hld_build(int x, int par=0, int head=0)
 	{
+		if (!head)
+			head = x;
+		h[x] = head;
 		pos[x] = ++sz;
-		if (head[cnt] == 0)
-			head[cnt] = x, l[cnt] = pos[x];
-		ind[x] = cnt;
-		r[cnt] = pos[x];
 		int mx = 0, u;
 		for (int to : v[x])
 		{
@@ -34,12 +33,11 @@ private:
 				mx = c[to], u = to;
 		}
 		if (mx > 0)
-			hld_build(u, x);
+			hld_build(u, x, head);
 		for (int to : v[x])
 		{
 			if (to == par || to == u)
 				continue;
-			cnt++;
 			hld_build(to, x);
 		}
 	}
@@ -48,26 +46,27 @@ public:
 	void init(int n = 1)
 	{
 		this->n = n;
+		sz = 0;
 		pos.resize(n + 1);
-		head.resize(n + 1);
-		ind.resize(n + 1);
-		l.resize(n + 1);
-		r.resize(n + 1);
+		h.resize(n + 1);
 		c.resize(n + 1);
 		pr.resize(n + 1);
 		v.resize(n + 1);
-		cnt = 1;
 	}
 	void add_edge(int x, int y)
 	{
 		v[x].push_back(y);
 		v[y].push_back(x);
 	}
-	void build()
+	void build(Type *a=NULL)
 	{
-		dfs(1, 0);
-		hld_build(1, 0);
+		dfs(1);
+		hld_build(1);
 		tree.build(n);
+		if (a == NULL)
+			return;
+		for (int i = 1; i <= n; i++)
+			tree.upd(pos[i], a[i]);
 	}
 	void upd(int x, Type k)
 	{
@@ -76,11 +75,11 @@ public:
 	Type get(int x, int y)
 	{
 		Type ans = -INF;
-		for(;ind[x] != ind[y]; y = pr[head[ind[y]]])
+		for(;h[x] != h[y]; y = pr[h[y]])
 		{
-			if (l[ind[x]] > l[ind[y]])
+			if (pos[h[x]] > pos[h[y]])
 				swap(x, y);
-			ans = max(ans, tree.get(l[ind[y]], pos[y]));
+			ans = max(ans, tree.get(pos[h[y]], pos[y]));
 		}
 		if(pos[x] > pos[y])
 			swap(x, y);
